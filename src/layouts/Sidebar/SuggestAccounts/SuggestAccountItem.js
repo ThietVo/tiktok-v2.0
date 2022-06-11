319f2e7e53e94ae27a8f25a28886c92c5cbc1fe0
ThietVo <vonhuthiet@gmail.com>
2022-06-11 16:17:13 +0700

    Sidebar: fix css for FollowingAccountItem and SuggestAccountItem

--- src/layouts/Sidebar/SuggestAccounts/SuggestAccountItem.js
+++ src/layouts/Sidebar/SuggestAccounts/SuggestAccountItem.js
@@ -1,52 +1,44 @@
-import { useRef, useState } from "react";
-import { FaCheckCircle } from "react-icons/fa";
-import clsx from "clsx";
-import { Link } from "react-router-dom";
+import { useRef, useState } from 'react';
+import { FaCheckCircle } from 'react-icons/fa';
+import clsx from 'clsx';
+import { Link } from 'react-router-dom';
 
-import styles from "./SuggestAccounts.module.scss";
+import styles from './SuggestAccounts.module.scss';
 import Avatar from '~/components/Avatar';
-import UserCard from "~/components/UserCard";
+import UserCard from '~/components/UserCard';
 
 function SuggestAccountItem({ user }) {
-  const [style, setStyle] = useState({ display: "none" });
+    const [style, setStyle] = useState({ display: 'none' });
 
-  let timerId = useRef();
+    let timerId = useRef();
 
-  const handleMouseEnter = () => {
-    timerId.current = setTimeout(() => setStyle({ display: "block" }), 1000);
-  };
-  const handleMouseLeave = () => {
-    clearTimeout(timerId.current);
+    const handleMouseEnter = () => {
+        timerId.current = setTimeout(() => setStyle({ display: 'block' }), 1000);
+    };
+    const handleMouseLeave = () => {
+        clearTimeout(timerId.current);
 
-    setStyle({ ...style, display: "none" });
-  };
+        setStyle({ ...style, display: 'none' });
+    };
 
-  return (
-    <div
-      className={styles.user}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <Link to={`/@${user.tiktokid}`} className={styles.userLink} key={user.id}>
-        <Avatar urlImg={user.avatar} avatarSmall />
+    return (
+        <div className={styles.user} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <Link to={`/@${user.tiktokid}`} className={styles.userLink} key={user.id}>
+                <Avatar urlImg={user.avatar} avatarSmall />
 
-        <div className={styles.userContent}>
-          <div className="d-flex">
-            <p className={styles.userContentTitle}>{user.tiktokid}</p>
-            {user.verified ? (
-              <FaCheckCircle className={clsx("checkIconSmall")} />
-            ) : (
-              ""
-            )}
-          </div>
-          <p className={styles.userContentDescription}>{user.username}</p>
+                <div className={styles.userContent}>
+                    <div className="d-flex">
+                        <p className={styles.userContentTitle}>{user.tiktokid}</p>
+                        {user.verified && <FaCheckCircle className={clsx('checkIconSmall')} />}
+                    </div>
+                    <p className={styles.userContentDescription}>{user.username}</p>
+                </div>
+            </Link>
+            <div className={styles.userCard}>
+                <UserCard style={style} user={user} />
+            </div>
         </div>
-      </Link>
-      <div className={styles.userCard}>
-        <UserCard style={style} user={user} />
-      </div>
-    </div>
-  );
+    );
 }
 
 export default SuggestAccountItem;
