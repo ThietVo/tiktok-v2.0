71d0f7ef76f528d4003f19689d411e0988930881
ThietVo <vonhuthiet@gmail.com>
2022-06-12 15:39:42 +0700

    Search: fix css and Link

--- src/components/AccountItem/AccountItem.js
+++ src/components/AccountItem/AccountItem.js
@@ -1,5 +1,6 @@
 import clsx from 'clsx';
 import { FaCheckCircle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import Avatar from '../Avatar';
 import styles from './AccountItem.module.scss';
@@ -7,16 +8,16 @@ import styles from './AccountItem.module.scss';
 function AccountItem({ data }) {
     
     return (
-        <div className={styles.wrapper}>
+        <Link to={`/@${data.tiktokid}`} className={styles.wrapper}>
             <Avatar urlImg={data.avatar} avatarSmallPlus/>
             <div className={styles.info}>
-                <h4 className={styles.infoTiktokid}>
-                    <span>{data.tiktokid}</span>
-                    <FaCheckCircle className={clsx("checkIconSmall")} />
-                </h4>
+                <div className="d-flex" >
+                    <p className={styles.infoTiktokid}>{data.tiktokid}</p>
+                    {data.verified && <FaCheckCircle className={clsx("checkIconSmall")} />}
+                </div>
                 <p className={styles.infoUsername}>{data.username}</p>
             </div>
-        </div>
+        </Link>
      );
 }
 
