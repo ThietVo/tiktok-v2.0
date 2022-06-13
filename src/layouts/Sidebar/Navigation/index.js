ea2fb1456976a2be82f4dd7ad113d9016bf176ed
ThietVo <vonhuthiet@gmail.com>
2022-06-13 16:12:57 +0700

    Sidebar: Navigation - fix css

--- src/layouts/Sidebar/Navigation/index.js
+++ src/layouts/Sidebar/Navigation/index.js
@@ -1,27 +1,66 @@
-import styles from "./Navigation.module.scss";
-import { FaHome, FaUserFriends, FaVideo } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import styles from './Navigation.module.scss';
+import { FaHome, FaUserFriends, FaVideo } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 function Navigation() {
-  return (
-    <div className={styles.nav}>
-      <Link
-        to="/"
-        className={styles.navLink}
-      >
-        <FaHome className={styles.navLinkIcon} />
-        <p>Dành cho bạn</p>
-      </Link>
-      <Link to="/following" className={styles.navLink}>
-        <FaUserFriends className={styles.navLinkIcon} />
-        <p>Đang Follow</p>
-      </Link>
-      <Link to="/live" className={styles.navLink}>
-        <FaVideo className={styles.navLinkIcon} />
-        <p>LIVE</p>
-      </Link>
-    </div>
-  );
+    const [active, setActive] = useState({
+        isHome: true,
+        isFollowing: false,
+        isLive: false,
+    });
+    const handleClickHome = () => {
+        setActive({
+            isHome: true,
+            isFollowing: false,
+            isLive: false,
+        });
+    };
+
+    const handleClickFollowing = () => {
+        setActive({
+            isHome: false,
+            isFollowing: true,
+            isLive: false,
+        });
+    };
+
+    const handleClickLive = () => {
+        setActive({
+            isHome: false,
+            isFollowing: false,
+            isLive: true,
+        });
+    };
+    return (
+        <div className={styles.nav}>
+            <Link
+                to="/"
+                className={clsx(styles.navLink, { [styles.navLinkActive]: active.isHome })}
+                onClick={handleClickHome}
+            >
+                <FaHome className={styles.navLinkIcon} />
+                <p>Dành cho bạn</p>
+            </Link>
+            <Link
+                to="/following"
+                className={clsx(styles.navLink, { [styles.navLinkActive]: active.isFollowing })}
+                onClick={handleClickFollowing}
+            >
+                <FaUserFriends className={styles.navLinkIcon} />
+                <p>Đang Follow</p>
+            </Link>
+            <Link
+                to="/live"
+                className={clsx(styles.navLink, { [styles.navLinkActive]: active.isLive })}
+                onClick={handleClickLive}
+            >
+                <FaVideo className={styles.navLinkIcon} />
+                <p>LIVE</p>
+            </Link>
+        </div>
+    );
 }
 
 export default Navigation;
