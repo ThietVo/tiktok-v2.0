795f37b256a81d928e605ba3aaa9271fdf36ac7d
ThietVo <vonhuthiet@gmail.com>
2022-06-22 22:17:17 +0700

    fix bugs Home pages

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,29 +1,28 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import styles from './Home.module.scss';
 import HomeItem from '~/components/HomeItem';
 import videosSlice from '~/redux/videosSlice';
 import { getVideosWithUsersApi } from '~/callApi/videosApi';
-import { usersSelector } from '~/redux/selectors';
+import { usersSelector, videosSelector } from '~/redux/selectors';
 import layoutSlice from '~/redux/layoutSlice';
 // import MobileHeader from "../MobileResponsive/MobileHeader";
 
 function Home() {
     const dispatch = useDispatch();
     const { userLogged } = useSelector(usersSelector);
-    const [videosWithUsers, setVideosWithUsers] = useState([]);
+    const { videosWithUsers } = useSelector(videosSelector);
 
     useEffect(() => {
         getVideosWithUsersApi().then((result) => {
             const arr = result.filter((element) => element.hasPublic === true && element.user.id !== userLogged.id);
-            setVideosWithUsers(arr.sort(() => Math.random() - 0.5));
-            dispatch(videosSlice.actions.setVideosWithUsers(arr));
+            dispatch(videosSlice.actions.setVideosWithUsers(arr.sort(() => Math.random() - 0.5)));
         });
         document.title = 'Tiktok - Make Your Day';
 
         dispatch(layoutSlice.actions.setHasClassContainer(true));
-    }, []);
+    }, [userLogged.id]);
 
     return (
         <div className={styles.homeMain}>
