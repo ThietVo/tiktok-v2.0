d2c12165f83fb35e6045ced31611ca043c9b6404
ThietVo <vonhuthiet@gmail.com>
2022-06-22 20:30:59 +0700

    fix bugs ToastMessage

--- src/components/ToastMessage/ToastMessage.js
+++ src/components/ToastMessage/ToastMessage.js
@@ -1,7 +1,7 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import modalSlice from "~/redux/modalSlice";
-import styles from "./ToastMessage.module.scss";
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import modalSlice from '~/redux/modalSlice';
+import styles from './ToastMessage.module.scss';
 
 function ToastMessage() {
     const dispatch = useDispatch();
@@ -9,12 +9,14 @@ function ToastMessage() {
     useEffect(() => {
         setTimeout(() => {
             dispatch(modalSlice.actions.setToastMessage(false));
-        }, 1000)
-    }, [])
+        }, 1000);
+    }, []);
 
-  return (
-    <div className={styles.toastMessage}>Đã xóa</div>
-  )
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.toastMessage}>Đã xóa</div>
+        </div>
+    );
 }
 
 export default ToastMessage;
