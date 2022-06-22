e501fa445a133e0d28aeb6e3c31a501944e4adfa
ThietVo <vonhuthiet@gmail.com>
2022-06-22 10:27:08 +0700

    build UI settings pages

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { BiUser } from 'react-icons/bi';
 import { AiOutlineLock } from 'react-icons/ai';
 import { IoMdNotificationsOutline } from 'react-icons/io';
+import { BsChevronRight } from 'react-icons/bs';
 import layoutSlice from '~/redux/layoutSlice';
 import styles from './Settings.module.scss';
 import Button from '~/components/Button';
@@ -35,57 +36,72 @@ function Settings() {
                 </div>
             </div>
             <div className={styles.content}>
-                <div className={styles.accountManager}>
+                <div className={styles.contentItem}>
                     <div className={styles.header}>Quản lý tài khoản</div>
-                    <div>
+                    <div className={styles.subItem}>
                         <div className={styles.title}>Kiểm soát tài khoản</div>
-                        <div className={styles.subTitle}>Xóa Tài khoản</div>
+                        <div className={styles.subTitle}>
+                            <span>Xóa Tài khoản</span>
+                            <div className={styles.subTitleRight}>Xóa</div>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.privacy}>
+                <div className={styles.contentItem}>
                     <div className={styles.header}>Quyền riêng tư</div>
-                    <div>
+                    <div className={styles.subItem}>
                         <div className={styles.title}>Khám phá</div>
-                        <div className={styles.subTitle}>Tài khoản riêng tư</div>
+                        <div className={styles.subTitle}>
+                            <span>Tài khoản riêng tư</span>
+                            <label className={styles.switchBtn}>
+                                <input type="checkbox" className={styles.switchBtnInput} />
+                                <span className={styles.switchBtnSlider}></span>
+                            </label>
+                        </div>
                         <div className={styles.desc}>
                             Với tài khoản riêng tư, chỉ những người dùng được bạn phê duyệt mới có thể follow bạn và xem
                             các video của bạn. Những người follow hiện tại sẽ không bị ảnh hưởng.
                         </div>
                     </div>
 
-                    <div>
-                        <div className={styles.subTitle}>Tài khoản bị chặn</div>
+                    <div className={styles.subItem}>
+                        <div className={styles.subTitle}>
+                            <span>Tài khoản bị chặn</span>
+                        </div>
+                        <BsChevronRight className={styles.subItemIcon} />
                     </div>
 
-                    <div>
+                    <div className={styles.subItem}>
                         <div className={styles.title}>Dữ liệu</div>
                         <div className={styles.subTitle}>Tải dữ liệu của bạn về</div>
                         <div className={styles.desc}>Lấy bản sao dữ liệu Tiktok cá nhân</div>
+                        <BsChevronRight className={styles.subItemIcon} />
                     </div>
                 </div>
-                <div className={styles.notification}>
+                <div className={styles.contentItem}>
                     <div className={styles.header}>Thông báo đẩy</div>
-                    <div>
+                    <div className={styles.subItem}>
                         <div className={styles.title}>Thông báo trên máy tính để bàn</div>
-                        <div className={styles.subTitle}>Cho phép trong trình duyệt</div>
+                        <div className={styles.subTitle}>
+                            <span>Cho phép trong trình duyệt</span>
+                            <label className={styles.switchBtn}>
+                                <input type="checkbox" className={styles.switchBtnInput} />
+                                <span className={styles.switchBtnSlider}></span>
+                            </label>
+                        </div>
                         <div className={styles.desc}>
                             Luôn cập nhật các thông báo về lượt thích, bình luận, video mới nhất và nhiều điều khác trên
                             máy tính. Bạn có thể tắt thông báo bất cứ lúc nào.
                         </div>
                     </div>
 
-                    <div>
+                    <div className={styles.subItem}>
                         <div className={styles.title}>Tùy chọn của bạn</div>
-                        <div className={styles.desc}>
-                        Tùy chọn của bạn sẽ được tự động đồng bộ với ứng dụng TikTok.
-                        </div>
+                        <div className={styles.desc}>Tùy chọn của bạn sẽ được tự động đồng bộ với ứng dụng TikTok.</div>
                     </div>
 
-                    <div>
+                    <div className={styles.subItem}>
                         <div className={styles.subTitle}>Tương tác</div>
-                        <div className={styles.desc}>
-                            Lượt thích, bình luận, follower mới, lượt nhắc đến
-                        </div>
+                        <div className={styles.desc}>Lượt thích, bình luận, follower mới, lượt nhắc đến</div>
                     </div>
                 </div>
             </div>
