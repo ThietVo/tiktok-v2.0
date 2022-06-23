7995a1d35bf0f68b69653c7d83d41c56867371d2
ThietVo <vonhuthiet@gmail.com>
2022-06-23 23:07:55 +0700

    responsive mobile #2: Header

--- src/layouts/HeaderOnlyLayout/HeaderOnlyLayout.js
+++ src/layouts/HeaderOnlyLayout/HeaderOnlyLayout.js
@@ -6,15 +6,11 @@ import styles from './HeaderOnlyLayout.module.scss';
 
 function HeaderOnlyLayout({ children }) {
     return ( 
-        <div>
+        <>
             <Modal />
             <Header />
-            <div className={styles.container}>
-                <div className={styles.content}>
-                    { children }
-                </div>
-            </div>
-        </div>
+            { children }
+        </>
      );
 }
 
